bdca1b4bb6bddb595005dfdf1e13822620bed1b0
Zyhler <icyxgram@gmail.com>
2023-01-17 01:25:02 +0100

    updated bot.py still not working
    Still not working. and need to rebuild both remind user and onactivityUpdate

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ import discord
 import asyncio
 import datetime
 import os
+import re
+from datetime import *
+
+
 # intents = discord.Intents.default()
 # intents.message_content = True
 client = discord.Client(intents=discord.Intents.default())
@@ -9,7 +13,6 @@ client = discord.Client(intents=discord.Intents.default())
 TOKEN = os.environ['DISCORD_TOKEN']
 # client = discord.Client()
 alarms = {}
-
 
 @client.event
 async def on_ready():
@@ -19,64 +22,71 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("!alarm"):
-        # time = message.content[6:] # get the time specified in the command
-        # time = datetime.strptime(time, "%H:%M") # convert the time string to a datetime object
+        
         user = message.author
         alarm_time = message.content[6:]
         # cmd, alarm_time = message.content.split(" ", 1)
-        alarm_time = datetime.strptime(alarm_time, "%H:%M")
-        alarm_time = alarm_time.replace(
-            year=datetime.now().year, month=datetime.now().month, day=datetime.now().day)
+        print(alarm_time)
+        alarm_datetime = datetime.now()
+        try:
+          alarm_hour,alarm_minutes = map(int,alarm_time.split(".",1))
+          alarm_datetime = alarm_datetime.replace(hour=alarm_hour,minute=alarm_minutes,second = 0)
+        except ValueError:
+          print("failed in 2nd try loop")
+          await message.channel.send(f"Alarm time is written like this 15.55 or 1.0 for 01.00")
+        
+       # alarm_datetime = alarm_datetime.replace(
+        #    year=datetime.now().year, month=datetime.now().month, day=datetime.now().day)
         user = message.author
         # if the alarm time is in the past, add one day to the alarm time
-        if alarm_time < datetime.now():
-            alarm_time += datetime.timedelta(days=1)
+        if alarm_datetime < datetime.now():
+            alarm_datetime += datetime.timedelta(days=1)
         # Create a unique identifier for the alarm
         alarm_id = f"{user.id}-{alarm_time}"
+        print(alarm_id)
+        print(user.id)
+        print(alarm_time)
         # If the alarm already exists, cancel it
         if alarm_id in alarms:
             alarms[alarm_id].cancel()
             del alarms[alarm_id]
-            await message.channel.send(f"Alarm for {alarm_time.strftime('%H:%M')} has been cancelled.")
+            await message.channel.send(f"Alarm for {alarm_datetime.strftime('%H:%M')} has been cancelled.")
         else:
             # Schedule the alarm
-            alarm = asyncio.ensure_future(
-                remind_user(user, alarm_time, alarm_id))
-            alarms[alarm_id] = alarm
-            await message.channel.send
-#
-        await message.channel.send(f"Alarm set for {alarm_time.strftime('%H:%M')}. Will be delayed if playing. - bound on activity.type")
-        # client.alarm_time = time
-        ''' check if the user is in a game 
-        if user.activity and user.activity.type == discord.ActivityType.playing:
-            await message.channel.send("I'll remind you when you're done playing.")
-            # set the alarm_time so that the on_activity_update event can use it.
-            client.alarm_time = time
-        else:
-            await message.channel.send("I'll remind you at the specified time.")
-            await asyncio.sleep((time - datetime.now()).seconds)
-            await message.channel.send(f"{user.mention}, it's {time.strftime('%H:%M')}!")
-'''
+           alarm = asyncio.ensure_future(
+                remind_user(user, alarm_datetime, alarm_id))
+           alarms[alarm_id] = alarm
+           await message.channel.send(f"Alarm set for {alarm_datetime.strftime('%H:%M')}. Will be delayed if playing. - bound on activity.type")
+        alarm = asyncio.ensure_future(
+                remind_user(user, alarm_datetime, alarm_id))
+        alarms[alarm_id] = alarm
 
 
 @client.event
 async def on_activity_update(before, after):
-    if after and before.type == discord.ActivityType.playing and after.type != discord.ActivityType.playing:
+    if after and before.activity.type == discord.ActivityType.playing and after.activity.type != discord.ActivityType.playing:
         user = after.user
         time = client.alarm_time
-        now = datetime.datetime.now()
+        now = datetime.now()
         if now > time:
             time += datetime.timedelta(days=1)
         time_to_sleep = (time - now).total_seconds()
         await asyncio.sleep(time_to_sleep)
-        await user.send(f"{user.mention}, it's {time.strftime('%H:%M')}!")
+        await user.send(f"{user.mention}, it's {datetime.strftime('%H:%M')}!")
 
 
-async def remind_user(user, time, alarm_id):
-    await asyncio.sleep((time - datetime.now()).seconds)
+async def remind_user(user, alarm_datetime, alarm_id):
+    print("remind_user runs")
+    print(alarms)
+    await asyncio.sleep((alarm_datetime - datetime.now()).seconds)
+    print("asyncio sleep is over")
     if alarm_id in alarms:
-        await user.send(f"{user.mention}, it's {time.strftime('%H:%M')}!")
+        print("if statement entered")
+        await user.send(f"{user.mention}, it's {alarm_datetime.strftime('%H:%M')}, go do what u set the alarm for!")
+        print(alarms[alarm_id])
+        alarms[alarm_id].cancel()
         del alarms[alarm_id]
+        
 
 
 client.run(TOKEN)
